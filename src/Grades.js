efe9a575b9c79b67d306871ab3b0ddc3c8f7da7e
deenhaque <fardeen2000@hotmail.com>
2021-04-29 18:19:44 -0400

    Barebones of the UI of the grade section. Still needs to be styled and have math done and stuff

---
+++ src/Grades.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Input() {
+  const [grades, setGrades] = useState([{weight: '', percent: ''}]);
+  const [del, setDel] = useState(false);
+
+  useEffect(()=> {
+    // If the delete flag is on, see if we can remove empty grades and remove them. Set the delete flag to false.
+    if (del) {
+      const newGrades = grades.filter(({weight, percent})=> weight || percent);
+      
+      if (JSON.stringify(newGrades) !== JSON.stringify(grades)) {
+        setGrades(newGrades);
+      }
+      setDel(false);
+    }
+    // If the last element is filled in, or if there is nothing in the list, add a new grade to grades
+    if (grades.length < 1 || grades[grades.length-1].weight || grades[grades.length-1].percent) {
+      setGrades(grades.concat([{weight: '', percent: ''}]));
+    }
+    return null;
+  }, [grades, del]);
+
+  return (
+    <div>
+      <h1>Grades</h1>
+      {grades.map((grade, i)=> {
+        const [weightKey, percentKey] = Object.keys(grade);
+        return (
+          <form key={`grade-${i}`}>
+            <div>
+              <input
+                type='text' name={`${weightKey}-${i}`}
+                value={grades[i].weight}
+                i={i} placeholder={weightKey}
+                onInput={e=> {
+                  setGrades(Array.from(grades, (grade, index)=> index===i ? {...grade, [weightKey]: e.target.value.replace(/\D/,'')} : grade));
+                  setDel(true);
+                }}
+              />
+              {console.log(<input type='text' name={`${weightKey}-${i}`} i={i} placeholder={weightKey}/>)}
+            </div>
+            <div>
+              <input
+                type='text' name={`${percentKey}-${i}`}
+                value={grades[i].percent}
+                i={i} placeholder={percentKey}
+                onInput={e=> {
+                  setGrades(Array.from(grades, (grade, index)=> index===i ? {...grade, [percentKey]: e.target.value.replace(/\D/,'')} : grade));
+                  setDel(true);
+                }}
+              />
+            </div>
+          </form>
+        );
+      })}
+    </div>
+  );
+}
