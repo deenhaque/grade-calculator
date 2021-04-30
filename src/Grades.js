7cb745a8140043d361ccd4e3d22df45e528ed4b6
deenhaque <fardeen2000@hotmail.com>
2021-04-30 16:52:45 -0400

    Basic style implementation set up for Grades, nothing really done yet

--- src/Grades.js
+++ src/Grades.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-export default function Input() {
+export default function Grades() {
+  // Styles
+  const Grade = styled.div`
+  `;
+  const StyledGrades = styled.div`
+    margin: auto;
+    width: 50%;
+  `;
+  const StyledInput = styled.input`
+    margin: 5px;
+  `;
   const [grades, setGrades] = useState([{weight: '', percent: ''}]);
   const [del, setDel] = useState(false);
+  const averageGrade = ()=> {
+    let avg = 0;
+    grades.forEach(({weight, percent})=> {
+      if (weight && percent) {
+        avg += weight*percent/100;
+      }
+    });
+    return avg;
+  }
 
   useEffect(()=> {
     // If the delete flag is on, see if we can remove empty grades and remove them. Set the delete flag to false.
@@ -22,37 +42,36 @@ export default function Input() {
   }, [grades, del]);
 
   return (
-    <div>
+    <StyledGrades>
       <h1>Grades</h1>
-      {grades.map((grade, i)=> {
-        const [weightKey, percentKey] = Object.keys(grade);
-        return (
-          <form key={`grade-${i}`}>
-            <div>
-              <input
-                type='text' name={`${weightKey}-${i}`}
-                value={grades[i].weight}
-                i={i} placeholder={weightKey}
-                onInput={e=> {
-                  setGrades(Array.from(grades, (grade, index)=> index===i ? {...grade, [weightKey]: e.target.value.replace(/\D/,'')} : grade));
-                  setDel(true);
-                }}
-              />
-            </div>
-            <div>
-              <input
-                type='text' name={`${percentKey}-${i}`}
-                value={grades[i].percent}
-                i={i} placeholder={percentKey}
-                onInput={e=> {
-                  setGrades(Array.from(grades, (grade, index)=> index===i ? {...grade, [percentKey]: e.target.value.replace(/\D/,'')} : grade));
-                  setDel(true);
-                }}
-              />
-            </div>
-          </form>
-        );
-      })}
-    </div>
+        {grades.map((grade, i)=> {
+          const [weightKey, percentKey] = Object.keys(grade);
+          return (
+              <Grade>
+                <StyledInput
+                  type='text' name={`${weightKey}-${i}`}
+                  value={grades[i].weight}
+                  i={i} placeholder={weightKey}
+                  onInput={e=> {
+                    setGrades(Array.from(grades, (grade, index)=> index===i ? {...grade, [weightKey]: e.target.value.replace(/\D/,'')} : grade));
+                    setDel(true);
+                  }}
+                />
+                <StyledInput
+                  type='text' name={`${percentKey}-${i}`}
+                  value={grades[i].percent}
+                  i={i} placeholder={percentKey}
+                  onInput={e=> {
+                    setGrades(Array.from(grades, (grade, index)=> index===i ? {...grade, [percentKey]: e.target.value.replace(/\D/,'')} : grade));
+                    setDel(true);
+                  }}
+                />
+              </Grade>
+          );
+        })}
+        <p>
+          {`${parseFloat(averageGrade()).toFixed(2)}%`}
+        </p>
+    </StyledGrades>
   );
 }
